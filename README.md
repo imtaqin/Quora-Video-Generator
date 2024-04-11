0fbbdd2b322b5b386f489a2c8dd5f4bba1528f60
fdciabdul <abdulmuttaqin456@gmail.com>
2024-04-11 12:57:23 +0700

    push new

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-"# Quora-Video-Generator" 
+Untuk menjalankan kode Node.js ini di Windows, menginstall FFmpeg, menginstall Node.js, membuat API OpenAI, dan mendapatkan TikTok session ID, ikuti langkah-langkah detail berikut:
+
+### 1. Cara Install Node.js di Windows:
+
+a. Kunjungi situs web resmi Node.js di [nodejs.org](https://nodejs.org/).
+b. Download versi "LTS" yang terbaru untuk Windows.
+c. Buka file installer yang telah didownload dan ikuti petunjuk instalasi.
+
+### 2. Cara Menjalankan File `main.js` di Windows:
+
+Langkah pertama setelah menginstall Node.js adalah memastikan bahwa Anda memiliki file `main.js` yang ingin dijalankan. Buka Command Prompt atau PowerShell, lalu navigasikan ke direktori dimana file `main.js` berada menggunakan perintah `cd`:
+
+```bash
+cd path\ke\direktori\file
+```
+
+Setelah itu, jalankan file dengan perintah:
+
+```bash
+node main.js
+```
+
+### 3. Cara Install FFmpeg di Windows:
+
+a. Download FFmpeg dari situs web resmi [FFmpeg.org](https://ffmpeg.org/download.html#build-windows).
+b. Ekstrak file ZIP yang didownload.
+c. Copy folder `bin`, `include`, dan `lib` dari folder yang diekstrak ke direktori yang Anda inginkan (biasanya `C:\FFmpeg`).
+d. Tambahkan `C:\FFmpeg\bin` ke variable environment `Path` sistem Windows:
+   - Buka Control Panel > System and Security > System.
+   - Klik "Advanced system settings" di sisi kiri.
+   - Klik "Environment Variables".
+   - Pada "System Variables", scroll dan temukan `Path`, kemudian klik "Edit".
+   - Klik "New" dan tambahkan path `C:\FFmpeg\bin`.
+   - Klik "OK" pada semua dialog yang terbuka untuk menyimpan perubahan.
+
+### 4. Cara Membuat API OpenAI dan Set API Key di `.env`:
+
+a. Kunjungi [OpenAI](https://openai.com/) dan buat akun atau log in.
+b. Setelah login, navigasi ke bagian API untuk membuat API key baru.
+c. Copy API key yang dihasilkan.
+
+Untuk menggunakan API Key di aplikasi Node.js Anda, buat file `.env` di root project Anda dan masukkan kunci API seperti berikut:
+
+```env
+OPENAI_API_KEY=YourOpenAI_ApiKey_Here
+```
+
+Ganti "YourOpenAI_ApiKey_Here" dengan API key yang Anda copy.
+
+### 5. Cara Mendapatkan Session ID TikTok dengan Ekstensi Cookie Editor Chrome Setelah Login TikTok:
+
+a. Install ekstensi Cookie Editor (atau cookie manager lainnya) di browser Chrome.
+b. Kunjungi website TikTok dan login ke akun Anda.
+c. Buka Cookie Editor dan cari cookies untuk situs TikTok.
+d. Cari cookie dengan nama `sessionid`, lalu copy nilai dari cookie tersebut.
+
+Untuk menggunakan session ID TikTok dalam aplikasi Anda, tambahkan ini ke file `.env` Anda:
+
+```env
+TIKTOK_SESSION_ID=YourTikTokSessionId_Here
+```
+
+Ganti "YourTikTokSessionId_Here" dengan session ID yang Anda copy.
+
+Setelah semua langkah di atas selesai, Anda bisa menjalankan kode Node.js Anda, dengan asumsi bahwa semua dependencies telah diinstall melalui `npm install`, dan environment sudah dikonfigurasi dengan benar
+
+
+
+## licensed by 
+
+@fdciabdul
+
+Project url : https://projects.co.id/user/my_projects/view/42af1b/quora-to-tiktok-video-generator
--- main.js
+++ main.js
@@ -1,46 +1,64 @@
 const fs = require("fs");
 const path = require("path");
+const inquirer = require('enquirer');
 const { config, createAudioFromText } = require('tiktok-tts');
 const { OpenAI } = require("openai");
 const QuoraExtractor = require("./src/lib/Quora.class");
 require('dotenv').config();
-config('3b4be950139a19b375148d26dd3df1b6');
+config(process.env.TIKTOK_SESSION_ID);
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 });
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "quoraURL",
+      message: "Please enter the Quora URL you would like to extract content from:",
+    },
+  ])
+  .then(async (answers) => {
+  
 
-async function saveAudioFromOpenAITTS(text, filePath) {
-    const mp3 = await openai.audio.speech.create({
+    async function saveAudioFromOpenAITTS(text, filePath) {
+      const mp3 = await openai.audio.speech.create({
         model: "tts-1-hd",
         voice: "nova",
         input: text,
-    });
+      });
 
-    const buffer = Buffer.from(await mp3.arrayBuffer());
-    await fs.promises.writeFile(filePath, buffer);
-    console.log(`${filePath} saved with OpenAI TTS.`);
-}
+      const buffer = Buffer.from(await mp3.arrayBuffer());
+      await fs.promises.writeFile(filePath, buffer);
+      console.log(`${filePath} saved with OpenAI TTS.`);
+    }
+    function removeEmojis(text) {
+      const emojiRegex = /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g;
+      return text.replace(emojiRegex, '');
+    }
+    async function saveTextAsSpeechUsingTikTok(text, filePath) {
+      await createAudioFromText(text, filePath, 'id_001');
+      console.log(`${filePath} saved with TikTok TTS.`);
+    }
 
-async function saveTextAsSpeechUsingTikTok(text, filePath) {
-    await createAudioFromText(text, filePath, 'id_001'); 
-    console.log(`${filePath} saved with TikTok TTS.`);
-}
-
-async function saveContentAsAudio(title, postContent, repliesText) {
-    const audioFolder = path.resolve(__dirname, "./audio");
-    if (!fs.existsSync(audioFolder)) {
+    async function saveContentAsAudio(title, postContent, repliesText) {
+      const audioFolder = path.resolve(__dirname, "./audio");
+      if (!fs.existsSync(audioFolder)) {
         fs.mkdirSync(audioFolder, { recursive: true });
-    }
-    await saveTextAsSpeechUsingTikTok(title, path.join(audioFolder, "title"));
-    for (let i = 0; i < repliesText.length; i++) {
-        await saveTextAsSpeechUsingTikTok(repliesText[i], path.join(audioFolder, `reply${i + 1}`));
+      }
+      
+      const sanitizedTitle = removeEmojis(title);
+      await saveTextAsSpeechUsingTikTok(sanitizedTitle, path.join(audioFolder, "title"));
+      
+      for (let i = 0; i < repliesText.length; i++) {
+        const sanitizedReply = removeEmojis(repliesText[i]);
+        await saveTextAsSpeechUsingTikTok(sanitizedReply, path.join(audioFolder, `reply${i + 1}`));
+      }
+
+      const sanitizedPostContent = removeEmojis(postContent);
+      await saveAudioFromOpenAITTS(sanitizedPostContent, path.join(audioFolder, "post.mp3"));
     }
 
-    await saveAudioFromOpenAITTS(postContent, path.join(audioFolder, "post.mp3"));
-}
-
-(async () => {
-    const extractor = new QuoraExtractor('https://id.quora.com/Apa-sisi-negatif-Nabi-Muhammad-SAW/answer/Agus-Joko-5?ch=10&oid=1477743708601115&share=c3d03134&srid=zWgiC&target_type=answer');
+    const extractor = new QuoraExtractor(answers.quoraURL);
     await extractor.init();
     const title = await extractor.extractTitle();
     const postContent = await extractor.extractPostContent();
@@ -50,4 +68,17 @@ async function saveContentAsAudio(title, postContent, repliesText) {
     console.log({ title, postContent, repliesText });
 
     await saveContentAsAudio(title, postContent, repliesText);
-})();
+
+    const { exec } = require("child_process");
+    exec("yt.bat", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  });
